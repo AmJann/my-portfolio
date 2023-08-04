358a80a76e348410b84fd2735fc943bb830ce6de
Amber Jankowski <97934285+AmJann@users.noreply.github.com>
2023-08-04 16:10:14 -0400

    added updated resume

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import resume from '../images/Amber-Jankowski-Aug2023.docx.pdf'
+import resume from '../images/Amber-Jankowski-Aug2023.pdf'
 import resumeIcon from '../images/resume-icon.png'
 
 function Contact() {
